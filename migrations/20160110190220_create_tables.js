47076aa9e0cfab16e0e3137253c7c2f319895780
davidtadams <adams.t.david@gmail.com>
2016-01-12 20:38:43 -0700

    adding email back to initial migration

--- migrations/20160110190220_create_tables.js
+++ migrations/20160110190220_create_tables.js
@@ -4,6 +4,7 @@ exports.up = function(knex, Promise) {
       table.increments('id').primary();
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
+      table.string('email');
       table.string('username', 20);
       table.string('google_id');
       table.string('photo_url', 500);
--- seeds/1-users.js
+++ seeds/1-users.js
@@ -1,18 +1,21 @@
 exports.seed = function(knex, Promise) {
     return Promise.all([
       knex('users').insert({
+        email: 'user1@gmail.com',
         username: 'user1',
         google_id: '45435345',
         photo_url: 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Puppy_on_Halong_Bay.jpg',
         personal_info: 'I am user1 and I love hiking in CO'
       }),
       knex('users').insert({
+        email: 'user2@gmail.com',
         username: 'user2',
         google_id: '489798347',
         photo_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrGybpSEQhxWFjG43fXyayot3bXIdaRAds6nIv4lMdSEFs21Vo',
         personal_info: 'I am user2 and I love hiking in UT'
       }),
       knex('users').insert({
+        email: 'user3@gmail.com',
         username: 'user3',
         google_id: '59860983',
         photo_url: 'http://www.pamperedpetz.net/wp-content/uploads/2015/09/Puppy1.jpg',
