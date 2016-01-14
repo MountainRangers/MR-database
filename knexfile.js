0fc2c3bfe284df8ff3aadf9546179661616a02b9
davidtadams <adams.t.david@gmail.com>
2016-01-14 00:01:34 -0700

    adding production environment to knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,14 @@ require('dotenv').load();
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DEV_DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.PROD_DATABASE_URL,
     migrations: {
       tableName: 'knex_migrations'
     }
